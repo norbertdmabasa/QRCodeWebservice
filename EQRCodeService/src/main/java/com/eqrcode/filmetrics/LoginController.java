89f9d70098e0830bec79c9e01e38e796410361de
norbertdmabasa <norbertdmabasa@gmail.com>
2017-11-29 17:53:46 +0800

    Login Class

---
+++ EQRCodeService/src/main/java/com/eqrcode/filmetrics/LoginController.java
@@ -0,0 +1,11 @@
+package com.eqrcode.filmetrics;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+	
+}
